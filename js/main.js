e93cff7f3287c4ae0b2152716114c9a9760c8c5f
reynaldi10 <samuelsaputrasitio@gmail.com>
2024-07-18 21:23:00 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,150 @@
+//Navigation bar effects on scroll
+window.addEventListener("scroll", function () {
+   const header = document.querySelector("header");
+   header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+//Service section - Modal
+const serviceModals = document.querySelectorAll(".services-modal");
+const learnmoreBtns = document.querySelectorAll(".learns-more-btn");
+const modalCloseBtns = document.querySelectorAll(".modals-close-btn");
+
+var modal = function (modalClick) {
+   serviceModals[modalClick].classList.add("active");
+};
+
+learnmoreBtns.forEach((learnmoreBtn, i) => {
+   learnmoreBtn.addEventListener("click", () => {
+      modal(i);
+   });
+});
+
+modalCloseBtns.forEach((modalCloseBtn) => {
+   modalCloseBtn.addEventListener("click", () => {
+      serviceModals.forEach((modalView) => {
+         modalView.classList.remove("active");
+      });
+   });
+});
+
+//Project section - Modal
+const projectModals = document.querySelectorAll(".project-model");
+const imgCards = document.querySelectorAll(".img-card");
+const projectCloseBtns = document.querySelectorAll(".project-close-btn");
+
+var modals = function (modalClick) {
+   projectModals[modalClick].classList.add("active");
+};
+
+imgCards.forEach((imgCard, i) => {
+   imgCard.addEventListener("click", () => {
+      modals(i);
+   });
+});
+
+projectCloseBtns.forEach((projectCloseBtn, i) => {
+   projectCloseBtn.addEventListener("click", () => {
+      projectModals.forEach((projectModalView) => {
+         projectModalView.classList.remove("active");
+      });
+   });
+});
+
+//Responsive navigation menu toggle
+const menuBtn = document.querySelector(".nav-menu-btn");
+const closeBtn = document.querySelector(".nav-close-btn");
+const navigation = document.querySelector(".navigation");
+const navItem = document.querySelectorAll(".nav-items a");
+
+menuBtn.addEventListener("click", () => {
+   navigation.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+   navigation.classList.remove("active");
+});
+
+navItem.forEach((navItem) => {
+   navItem.addEventListener("click", () => {
+      navigation.classList.remove("active");
+   });
+});
+
+//Scroll reveal animations
+//Common reveal options to create reveal animations
+ScrollReveal({
+   // reset: true,
+   distance: "60px",
+   duration: 2500,
+   delay: 100,
+});
+
+//Target elements, and specify options to create reveal animations
+ScrollReveal().reveal(".home .info h2, .section-title-01, .section-title-02", { delay: 500, origin: "left" });
+ScrollReveal().reveal(".home .info h3, .home .info p, .about-info .btn", { delay: 600, origin: "right" });
+ScrollReveal().reveal(".home .info .btn", { delay: 700, origin: "bottom" });
+ScrollReveal().reveal(".media-icons i, .contact-left li", { delay: 500, origin: "left", interval: 200 });
+ScrollReveal().reveal(".home-img, about-img", { delay: 500, origin: "bottom" });
+ScrollReveal().reveal(".about .description, .contact-right", { delay: 600, origin: "right" });
+ScrollReveal().reveal(".about .profesional-list li", { delay: 500, origin: "right", interval: 200 });
+ScrollReveal().reveal(".skills-description, service-description, .contact-card, contact-left h2", { delay: 700, origin: "left" });
+ScrollReveal().reveal(".experience-card, .service-card, .education, .project .img-card", { delay: 800, origin: "bottom", interval: 200 });
+ScrollReveal().reveal("footer .group", { delay: 500, origin: "top", interval: 200 });
+
+// Contact Form
+const form = document.querySelector("form");
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const subject = document.getElementById("subject");
+const mess = document.getElementById("message");
+
+function sendEmail() {
+   const bodyMessage = "Full Name: ${fullName.value}<br> Email: ${email.value}<br> Message: ${mess.value}";
+
+   Email.send({
+      Host: "smtp.elasticemail.com",
+      Username: "reynaldiagathahalohoaldi@gmail.com",
+      Password: "A1F504A652665AB49D2A4EAD28EF918EE6DB",
+      To: "reynaldiagathahalohoaldi@gmail.com",
+      From: document.getElementById("email").value,
+      Subject: subject.value,
+      Body: bodyMessage,
+   }).then((message) => {
+      if (message === "OK") {
+         Swal.fire({
+            title: "Berhasil!",
+            text: "Pesan berhasil terkirim",
+            icon: "success",
+         });
+      }
+   });
+}
+
+function checkInputs() {
+   const items = document.querySelectorAll(".item");
+
+   for (const item of items) {
+      if (item.value == "") {
+         item.classList.add("error");
+         item.parentElement.classList.add("error");
+      }
+
+      item.addEventListener("keyup", () => {
+         if (item.value != "") {
+            item.classList.remove("error");
+            item.parentElement.classList.remove("error");
+         } else {
+            item.classList.add("error");
+            item.parentElement.classList.add("error");
+         }
+      });
+   }
+}
+
+form.addEventListener("submit", (e) => {
+   e.preventDefault();
+   checkInputs();
+
+   sendEmail();
+   form.reset();
+});
